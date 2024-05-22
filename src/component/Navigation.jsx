23932c1cd6a3337cb6b886dabe710da2c8c05517
arpit <arpitsingh2801@gmail.com>
2024-05-22 16:35:44 +0530

    Brand-page

---
+++ src/component/Navigation.jsx
@@ -0,0 +1,26 @@
+const Navigation = () => {
+  return (
+    <nav className="container">
+      <div className="brand-logo">
+        <img src="/Images/brand_logo.png" alt="Nike brand logo"></img>
+      </div>
+      <ul>
+        <li>
+          <a href="#">Menu</a>
+        </li>
+        <li>
+          <a href="#">Location</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+      <button>Login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
